e7d2506b292d028cd853af7ddeb21807172a8d50
Russell Esperanza <resperanza11@gmail.com>
2020-06-23 14:27:20 -0700

    added functionality to page buttons

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const Pagination = (props) => {
-  let { reviewsPerPage, totalReviews, nextPage, changePage } = props;
+  let { reviewsPerPage, totalReviews, nextPage, previousPage, changePage } = props;
 
   const pageNumbers = [];
 
-  for (let i = 1; i < Math.ceil(totalReviews / reviewsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalReviews / reviewsPerPage); i++) {
     pageNumbers.push(i);
   }
 
   return (
     <div className="pagination">
-      <button aria-label="previous page of reviews">{'<'}</button>
+      <button aria-label="previous page of reviews" onClick={previousPage}>{'<'}</button>
       {pageNumbers.map((page, i) => (
-        <button key={i} aria-label={`page ${page}`}>{page}</button>
+        <button key={i} aria-label={`page ${page}`} onClick={() => changePage(page)}>{page}</button>
       ))}
-      <button aria-label="next page of reviews">{'>'}</button>
+      <button aria-label="next page of reviews" onClick={nextPage}>{'>'}</button>
     </div>
   );
 };
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -19,6 +19,9 @@ export default class App extends React.Component {
     this.getReviews = this.getReviews.bind(this);
     this.calculateAvgRating = this.calculateAvgRating.bind(this);
     this.paginate = this.paginate.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+    this.previousPage = this.previousPage.bind(this);
+    this.changePage = this.changePage.bind(this);
   }
 
   componentDidMount() {
@@ -61,12 +64,31 @@ export default class App extends React.Component {
     return reviews.slice(indexOfFirstReview, indexOfLastReview);
   }
 
-  nextPage(e) {
+  nextPage() {
+    let { reviews, reviewsPerPage, currentPage } = this.state;
+    let lastPage = Math.ceil(reviews.length / reviewsPerPage);
 
+    if (currentPage < lastPage) {
+      this.setState({
+        currentPage: this.state.currentPage + 1
+      });
+    }
   }
 
-  changePage(e) {
+  previousPage() {
+    let { currentPage } = this.state;
 
+    if (currentPage > 1) {
+      this.setState({
+        currentPage: this.state.currentPage - 1
+      });
+    }
+  }
+
+  changePage(page) {
+    this.setState({
+      currentPage: page
+    });
   }
 
   render() {
@@ -84,7 +106,13 @@ export default class App extends React.Component {
         {this.paginate().map((review, i) => (
           <ReviewEntry key={i} review={review} />
         ))}
-        <Pagination reviewsPerPage={this.state.reviewsPerPage} totalReviews={this.state.reviews.length} />
+        <Pagination
+          reviewsPerPage={this.state.reviewsPerPage}
+          totalReviews={this.state.reviews.length}
+          nextPage={this.nextPage}
+          previousPage={this.previousPage}
+          changePage={this.changePage}
+        />
       </div>
     );
   }
