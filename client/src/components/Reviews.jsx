4c2aa02120cd4cc67a35247d8c93aa4536d6136d
Russell Esperanza <resperanza11@gmail.com>
2020-06-23 11:24:04 -0700

    setup header for reviews module

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -8,17 +8,45 @@ export default class Reviews extends React.Component {
 
     this.state = {
       productId: 'handcrafted-metal-bike',
-      product: {}
+      reviews: [],
     };
+
+    this.getReviews = this.getReviews.bind(this);
   }
 
   componentDidMount() {
+    this.getReviews();
+  }
 
+  getReviews() {
+    let { productId } = this.state;
+
+    Axios.get(`/api/${productId}`)
+      .then(result => {
+        this.setState({
+          reviews: result.data[0].reviews
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   render() {
     return (
-      <div>Hello from React!</div>
+      <div id="Reviews" className="col-12 col-md-10 offset-md-1">
+        <div className="header">
+          <h3 className="h3">ALO FAM REVIEWS</h3>
+        </div>
+        <div className="subheader">
+          <div className="StarsUI">
+            <span className="fill" style={{width: "91.1112%"}}></span>
+          </div>
+          <span className="large-p">|</span>
+          <span className="large-p text">{this.state.reviews.length} Reviews</span>
+        </div>
+        <hr></hr>
+      </div>
     );
   }
 }
