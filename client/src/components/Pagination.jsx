df347a1eee25964f85525157ab70ae55dff47d2d
Russell Esperanza <resperanza11@gmail.com>
2020-06-23 16:11:09 -0700

    pagination is now fully functioning

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
 const Pagination = (props) => {
-  let { reviewsPerPage, totalReviews, nextPage, previousPage, changePage } = props;
+  let { reviewsPerPage, totalReviews, changePage, selectPage, currentPage } = props;
+  let lastPage = Math.ceil(totalReviews / reviewsPerPage);
 
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalReviews / reviewsPerPage); i++) {
+  for (let i = 1; i <= lastPage; i++) {
     pageNumbers.push(i);
   }
 
   return (
     <div className="pagination">
-      <button aria-label="previous page of reviews" onClick={previousPage}>{'<'}</button>
+      <button aria-label="previous page of reviews" className="option left" onClick={() => changePage()} disabled={currentPage === 1 ? 'disabled' : null}>{'<'}</button>
       {pageNumbers.map((page, i) => (
-        <button key={i} aria-label={`page ${page}`} onClick={() => changePage(page)}>{page}</button>
+        <button key={i} aria-label={`page ${page}`} className={currentPage === page ? 'option active' : 'option '} onClick={() => selectPage(page)}>{page}</button>
       ))}
-      <button aria-label="next page of reviews" onClick={nextPage}>{'>'}</button>
+      <button aria-label="next page of reviews" className="option right" onClick={() => changePage(true)} disabled={currentPage === lastPage ? 'disabled' : null}>{'>'}</button>
     </div>
   );
 };
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -19,9 +19,8 @@ export default class App extends React.Component {
     this.getReviews = this.getReviews.bind(this);
     this.calculateAvgRating = this.calculateAvgRating.bind(this);
     this.paginate = this.paginate.bind(this);
-    this.nextPage = this.nextPage.bind(this);
-    this.previousPage = this.previousPage.bind(this);
     this.changePage = this.changePage.bind(this);
+    this.selectPage = this.selectPage.bind(this);
   }
 
   componentDidMount() {
@@ -64,28 +63,13 @@ export default class App extends React.Component {
     return reviews.slice(indexOfFirstReview, indexOfLastReview);
   }
 
-  nextPage() {
-    let { reviews, reviewsPerPage, currentPage } = this.state;
-    let lastPage = Math.ceil(reviews.length / reviewsPerPage);
-
-    if (currentPage < lastPage) {
-      this.setState({
-        currentPage: this.state.currentPage + 1
-      });
-    }
+  changePage(next) {
+    this.setState({
+      currentPage: next ? this.state.currentPage + 1 : this.state.currentPage - 1
+    });
   }
 
-  previousPage() {
-    let { currentPage } = this.state;
-
-    if (currentPage > 1) {
-      this.setState({
-        currentPage: this.state.currentPage - 1
-      });
-    }
-  }
-
-  changePage(page) {
+  selectPage(page) {
     this.setState({
       currentPage: page
     });
@@ -93,26 +77,29 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div id="Reviews" className="col-12 col-md-10 offset-md-1">
-        <div className="header">
-          <h3 className="h3">ALO FAM REVIEWS</h3>
+      <div id="product-display" className="d-none d-md-block">
+        <div className="description__line"></div>
+        <div id="Reviews" className="col-12 col-md-10 offset-md-1">
+          <div className="header">
+            <h3 className="h3">ALO FAM REVIEWS</h3>
+          </div>
+          <div className="subheader">
+            <StarsUI rating={this.state.rating} />
+            <span className="large-p">|</span>
+            <span className="large-p text">{this.state.reviews.length} Reviews</span>
+          </div>
+          <hr></hr>
+          {this.paginate().map((review, i) => (
+            <ReviewEntry key={i} review={review} />
+          ))}
+          <Pagination
+            reviewsPerPage={this.state.reviewsPerPage}
+            totalReviews={this.state.reviews.length}
+            changePage={this.changePage}
+            selectPage={this.selectPage}
+            currentPage={this.state.currentPage}
+          />
         </div>
-        <div className="subheader">
-          <StarsUI rating={this.state.rating} />
-          <span className="large-p">|</span>
-          <span className="large-p text">{this.state.reviews.length} Reviews</span>
-        </div>
-        <hr></hr>
-        {this.paginate().map((review, i) => (
-          <ReviewEntry key={i} review={review} />
-        ))}
-        <Pagination
-          reviewsPerPage={this.state.reviewsPerPage}
-          totalReviews={this.state.reviews.length}
-          nextPage={this.nextPage}
-          previousPage={this.previousPage}
-          changePage={this.changePage}
-        />
       </div>
     );
   }
